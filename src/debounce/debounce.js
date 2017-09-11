307f2a581ce7f6751b9239e411840d20b85b0fd2
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2017-09-11 21:39:31 +0300

    Added jsdoc documentation

--- src/debounce/debounce.js
+++ src/debounce/debounce.js
@@ -1,4 +1,12 @@
-
+/**
+ * Method debounce creates Hi Ordered Function which sets minimal period between calls.
+ * @param {Function} func - original function.
+ * @param {Object} [config = {}] - configurin object.
+ * @param {number} [config#wait = 100] - minimal number of milliseconds to be waited between calls.
+ * @param {boolean} [config#immediate = false] - when immediate = true function calls immediatly if it is possible, in other case it cals after wait ms.
+ * @param {Object} [config#context] - context object of function. If context was set it can not be changed or removed.
+ * @return {Function} - decorated function.
+ */
 export default (func, config = {}) => {
   const { wait = 100, immediate = false, context } = config;
   let timeout;
